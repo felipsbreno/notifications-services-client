dcb4a26ceb3a7de49c45935ec4cf8368fda6eb4e
felipsbreno <brendo.filipe2050@gmail.com>
2022-12-22 23:23:19 -0300

    create header component and cards components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,70 @@
+import Link from 'next/link';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Grid,
+  Paper,
+  Container,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { GitHub } from '@mui/icons-material';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Wrapper = styled('div')(({}) => ({
+  marginTop: 60,
+}));
+
 export default function Home() {
-  return <h1>Notfications Service Client</h1>;
+  return (
+    <>
+      <HeaderApp />
+
+      <Wrapper>
+        <CardNotificationRender />
+      </Wrapper>
+    </>
+  );
 }
+
+const CardNotificationRender = () => (
+  <Container maxWidth="lg">
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <Item>Item 1</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>Item 1</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  </Container>
+);
+
+const HeaderApp = () => (
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Notifications Service Client
+        </Typography>
+
+        <IconButton size="large" color="inherit">
+          <Link href="https://github.com/" target="_blank">
+            <GitHub />
+          </Link>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  </Box>
+);
